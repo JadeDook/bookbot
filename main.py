78c953995f57253a03f5588bc971d89268e13988
jade <dicerule@gmail.com>
2025-11-29 09:48:30 -0800

    Added get_num_characters function to stats

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, get_num_characters
 
 def get_book_text(filepath) -> str:
     file_contents = ""
@@ -9,6 +9,8 @@ def get_book_text(filepath) -> str:
 def main():
     book_text = get_book_text('books/frankenstein.txt')
     word_count = count_words(book_text)
+    character_count = get_num_characters(book_text)
     print(word_count)
+    print(character_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def count_words(textfile):
     count = len(textfile.split())
     return f"Found {count} total words"
+
+def get_num_characters(textfile):
+    char_count = {}
+    for char in textfile:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
